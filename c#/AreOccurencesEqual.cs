c9dc40b41ee1148eb969021f47ae3a9cb281ed9d
Burcu Agdere <burcuagdere@gmail.com>
2024-01-24 21:23:05 +0000

    IntersectionOfMultipleArrays and AreOccurencesEqual

---
+++ c#/AreOccurencesEqual.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Solution
+{
+    public bool AreOccurrencesEqual(string s)
+    {
+        Dictionary<char, int> counts = new Dictionary<char, int>();
+        HashSet<int> occurences = new HashSet<int>();
+
+        foreach (char c in s)
+        {
+            counts[c] = counts.GetValueOrDefault(c, 0) + 1;
+        }
+
+        foreach (char key in counts.Keys)
+        {
+            occurences.Add(counts[key]);
+        }
+
+        return occurences.Count == 1;
+    }
+}
---
+++ c#/IntersectionOfMultipleArrays.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Solution
+{
+    public IList<int> Intersection(int[][] nums)
+    {
+        var counts = new Dictionary<int, int>();
+        var ans = new List<int>();
+        foreach (var arr in nums)
+        {
+            foreach (var num in arr)
+            {
+                counts[num] = counts.GetValueOrDefault(num, 0) + 1;
+            }
+        }
+        var n = nums.Length;
+        foreach (var key in counts.Keys)
+        {
+            if (counts[key] == n)
+            {
+                ans.Add(key);
+            }
+        }
+        ans.Sort();
+        return ans.ToArray();
+    }
+}
